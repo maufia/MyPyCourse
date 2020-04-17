d5d8e9f72e760579cdcd537ad670ead9f982cdcf
Mauro Fiacco <github@fiacco.org>
2020-04-17 09:14:32 +0100

    Better handle of choices!

--- Learn_EasyGui.py
+++ Learn_EasyGui.py
@@ -7,34 +7,35 @@ import click
 ABOUT = """Learn and examples with EasyGUI"""
 
 
-def use_gui_to_select_file() -> True:
+def my_function() -> True:
+    """Call error message
+    :return: True
+    """
+    print("You have call me!")
+    show_error_message("The function 'my_function' has raised and error")
+    return True
+
+
+def show_error_message(error_string="Error!") -> True:
+    """
+    A message box can be used for alert of error, success, etc
+    return: True
+    """
+    eg.msgbox(title="Learn_EasyGUI: Example Error",
+              image=os.path.join('Images', 'failure.gif'),
+              msg=error_string)
+    return True
+
+
+def select_file() -> True:
     """Use EasyGUI to select a function"""
     current_directory = os.getcwd()
-    selected_file = eg.fileopenbox(title='Open a file',
+    selected_file = eg.fileopenbox(title='Learn_EasyGUI: Open a file',
                                    default=os.path.join(current_directory, ".."),
                                    filetypes="*.txt,*.py")
     print(f"Selected file: {os.path.basename(selected_file)}")
     print(f"In directory: {os.path.dirname(selected_file)}")
     return True
-
-
-def show_error_message() -> True:
-    """
-    A message box can be used for alert of error, success, etc
-    return: True
-    """
-    eg.msgbox(title="Example Error",
-              image=os.path.join('Images', 'failure.gif'),
-              msg='Error message!\n\n No no no and no!')
-    return True
-
-
-def show_demos() -> True:
-    """
-    call Easy Gui demo, all type of widgets are shown
-    :return: True
-    """
-    eg.egdemo()
 
 
 def input_argument_handler():
@@ -43,20 +44,32 @@ def input_argument_handler():
         """Main call to the CLI"""
         pass
 
-    @cli.command(help="Open a file")
-    def select_file():
-        """Call select file"""
-        use_gui_to_select_file()
+    @cli.command('choices', help="Select an option")
+    def handle_choices() -> True:
+        """
+        Selects an option from list, and call a related functionality
+        :return: True
+        """
+        # This is a 'clever' technique!
+        # Define the strings you need to use in a dictionaty,
+        # and associate each string to a function name
+        all_choices = {'Run my\nfunction': my_function,
+                       'Select a\nfile': select_file}
 
-    @cli.command(help="Error message")
-    def error_message():
-        """Call error message"""
-        show_error_message()
+        # Use Gui to select a choice
+        choice = eg.buttonbox(msg="Select and action",
+                              title='Learn_EasyGui',
+                              choices=list(all_choices.keys()),
+                              image=os.path.join('Images', 'qm.png'))
 
-    @cli.command(help="EasyGui demos")
-    def all_demos():
+        # This is the clever bit!! Run the choice as a function
+        all_choices[choice]()
+        return True
+
+    @cli.command('demos', help="EasyGui demos")
+    def show_demos():
         """Call easygui demos"""
-        show_demos()
+        eg.egdemo()
 
     cli(obj={})
 
